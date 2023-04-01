ac157eb2a0085e6353eb0c212b1f10132b88e511
sturmfx <ttunikitashtobert@gmail.com>
2023-04-01 17:10:20 +0300

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -6,31 +6,31 @@ t = turtle.Turtle()
 t.speed('slowest')
 
 
-def draw_human():
+def draw_human(head, neck, torso, legs, hands):
     # рисование головы
-    t.circle(25)
+    t.circle(head)
     # рисование шеи
     t.right(90)
-    t.forward(25)
+    t.forward(neck)
     # рисование левой руки
     t.right(90)
-    t.forward(35)
+    t.forward(hands)
     # рисование правой руки
     t.right(180)
-    t.forward(70)
+    t.forward(hands * 2)
     # рисование торса
     t.right(180)
-    t.forward(35)
+    t.forward(hands)
     t.right(270)
-    t.forward(50)
+    t.forward(torso)
     # рисование левой ноги
     t.right(45)
-    t.forward(40)
+    t.forward(legs)
     # рисование правой ноги
     t.right(180)
-    t.forward(40)
+    t.forward(legs)
     t.right(90)
-    t.forward(40)
+    t.forward(legs)
     # возвращаем напраление черепашки направо
     t.left(45)
 
@@ -63,7 +63,7 @@ def on_click(x, y):
     t.penup()
     t.goto(x, y)
     t.pendown()
-    forest()
+    draw_human(25, 25, 50,40,35)
 
 
 c.onscreenclick(on_click)
